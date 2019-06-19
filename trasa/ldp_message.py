fff91c5c54a8d5407512a03e7a69a0c33593bc9a
Sam Russell <sam.h.russell@gmail.com>
2019-06-19 16:14:08 +1200

    Pull common logic out of LDP message classes
    Hello and Initialisation messages done, contributes to #1

--- trasa/ldp_message.py
+++ trasa/ldp_message.py
@@ -6,6 +6,8 @@ from .ip import IP6Prefix, IP6Address
 from .chopper import Chopper
 from .tlv import parse_tlv, pack_tlv
 from io import BytesIO
+from itertools import chain
+from collections import OrderedDict
 
 class LdpMessage(object):
     HELLO_MESSAGE = 0x100
@@ -68,7 +70,7 @@ def register_parser(cls):
     return cls
 
 def parse_tlvs(serialised_tlvs):
-    return dict([parse_tlv(x) for x in Chopper(4, 2, 0, BytesIO(serialised_tlvs))])
+    return OrderedDict([parse_tlv(x) for x in Chopper(4, 2, 0, BytesIO(serialised_tlvs))])
 
 def pack_tlvs(tlvs):
     return b"".join([pack_tlv(key, value) for key, value in tlvs.items()])
@@ -83,27 +85,11 @@ class LdpHelloMessage(LdpMessage):
 
     @classmethod
     def parse(cls, serialised_message):
-        message_id, = struct.unpack(
-            "!I",
-            serialised_message[:4]
-        )
-        serialised_tlvs = serialised_message[4:]
-        tlvs = parse_tlvs(serialised_tlvs)
-        return cls(message_id, tlvs)
+        generic_message = LdpGenericMessage.parse(cls.MSG_TYPE, serialised_message)
+        return cls(generic_message.message_id, generic_message.tlvs)
 
     def pack(self):
-        packed_message_body = struct.pack(
-            "!I",
-            self.message_id
-        ) + pack_tlvs(self.tlvs)
-        message_length = len(packed_message_body)
-        packed_message_header = struct.pack(
-            "!HH",
-            self.MSG_TYPE,
-            message_length
-        )
-
-        return packed_message_header + packed_message_body
+        return LdpGenericMessage(self.MSG_TYPE, self.message_id, self.tlvs).pack()
 
     def __str__(self):
         return "LdpHelloMessage: ID: %s, TLVs: %s" % (
@@ -125,44 +111,31 @@ class LdpInitialisationMessage(LdpMessage):
         self.receiver_ldp_identifier = receiver_ldp_identifier
         self.tlvs = tlvs
 
-    @classmethod
-    def parse(cls, serialised_message):
-        message_id, = struct.unpack(
-            "!I",
-            serialised_message[:4]
-        )
-        serialised_tlvs = serialised_message[4:]
-        tlvs = parse_tlvs(serialised_tlvs)
-
-        # handle common TLVs
-        common_session_parameters = tlvs.pop(0x0500)
-        protocol_version, keepalive_time, flags, path_vector_limit, max_pdu_length, receiver_ldp_identifier = struct.unpack(
-            "!HHBBH6s", common_session_parameters
-        )
-
-        return cls(message_id, protocol_version, keepalive_time, flags, path_vector_limit, max_pdu_length, receiver_ldp_identifier, tlvs)
-
-    def pack(self):
+    def build_common_tlvs(self):
         # handle common TLVs
         common_session_parameters = struct.pack(
             "!HHBBH6s",
             self.protocol_version, self.keepalive_time, self.flags, self.path_vector_limit, self.max_pdu_length, self.receiver_ldp_identifier
         )
 
-        common_session_parameters_tlv = pack_tlv(0x0500, common_session_parameters)
+        return OrderedDict([(0x0500, common_session_parameters)])
 
-        packed_message_body = struct.pack(
-            "!I",
-            self.message_id
-        ) + common_session_parameters_tlv + pack_tlvs(self.tlvs)
-        message_length = len(packed_message_body)
-        packed_message_header = struct.pack(
-            "!HH",
-            self.MSG_TYPE,
-            message_length
+    @classmethod
+    def parse(cls, serialised_message):
+        generic_message = LdpGenericMessage.parse(cls.MSG_TYPE, serialised_message)
+
+        # handle common TLVs
+        common_session_parameters = generic_message.tlvs.pop(0x0500)
+        protocol_version, keepalive_time, flags, path_vector_limit, max_pdu_length, receiver_ldp_identifier = struct.unpack(
+            "!HHBBH6s", common_session_parameters
         )
 
-        return packed_message_header + packed_message_body
+        return cls(generic_message.message_id, protocol_version, keepalive_time, flags, path_vector_limit, max_pdu_length, receiver_ldp_identifier, generic_message.tlvs)
+
+    def pack(self):
+        combined_tlvs = OrderedDict(chain(self.build_common_tlvs().items(), self.tlvs.items()))
+
+        return LdpGenericMessage(self.MSG_TYPE, self.message_id, combined_tlvs).pack()
 
     def __str__(self):
         return "LdpInitialisationMessage: ID: %s, Protocol version: %s, Keepalive time: %s, Flags: %s, PVLim: %s, Max PDU Length: %s, Receiver LDP ID: %s, TLVs: %s" % (
